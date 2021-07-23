e21af2066c53e2fc3f58aa9824dcb3acb2163c45
Raza Rahat Khan <raza.rahatkhan@Fiserv.com>
2021-07-23 11:37:13 -0400

    initial commit

---
+++ Learning git2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Learning_git2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Learning git step 2");
+        }
+    }
+}
